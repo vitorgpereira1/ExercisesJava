cd3a328715171db1312cfdf913442349dce5ff8a
Vitor Pereira <vitorgp14@hotmail.com>
2024-02-22 11:10:00 -0300

    new exercise

---
+++ src/exercises/conditional_structure/exercise4.java
@@ -0,0 +1,24 @@
+package exercises.conditional_structure;
+
+import java.util.Scanner;
+
+public class exercise4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("The game started at: ");
+        int itStarted = scanner.nextInt();
+        System.out.println("The game ended at: ");
+        int itEnded = scanner.nextInt();
+
+        int duration;
+        if (itStarted < itEnded) {
+            duration = itEnded - itStarted;
+        } else {
+            duration = 24 - itStarted + itEnded;
+        }
+
+        System.out.println("O jogo durou " + duration + " hora(s)!");
+        scanner.close();
+    }
+}
---
+++ src/exercises/conditional_structure/exercise5.java
@@ -0,0 +1,45 @@
+package exercises.conditional_structure;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class exercise5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+        //seguir, calcule e mostre o valor da conta a pagar
+
+        /*Itens:
+        1 - Hotdog = 4,00
+        2 - X-Salad = 4,50
+        3 - X-Bacon = 5,00
+        4 - Simple toast = 2,00
+        5 - Soda = 1,50
+
+        */
+
+        System.out.println("Item code: ");
+        int code = scanner.nextInt();
+
+        System.out.println("Item quantity: ");
+        int quantity = scanner.nextInt();
+
+        double amountToPay = 0;
+        if (code == 1){
+            amountToPay = quantity * 4.00;
+        } else if (code == 2) {
+            amountToPay = quantity * 4.50;
+        } else if (code == 3) {
+            amountToPay = quantity * 5.00;
+        } else if (code == 4) {
+            amountToPay = quantity * 2.00;
+        } else if (code == 5) {
+            amountToPay = quantity * 1.50;
+        }
+
+        System.out.printf("Total: R$ %.2f", amountToPay);
+
+        scanner.close();
+    }
+}
