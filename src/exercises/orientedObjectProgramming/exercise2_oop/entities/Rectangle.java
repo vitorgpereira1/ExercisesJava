8ab9863644f0d4be8a860d410611086b6f855306
Vitor Pereira <vitorgp14@hotmail.com>
2024-02-29 10:23:05 -0300

    new exercise oop

---
+++ src/exercises/orientedObjectProgramming/exercise2_oop/entities/Rectangle.java
@@ -0,0 +1,16 @@
+package exercises.orientedObjectProgramming.exercise2_oop.entities;
+
+public class Rectangle {
+    public double widht;
+    public double height;
+
+    public double area (){
+        return widht * height;
+    }
+    public double perimeter (){
+        return 2 * (widht + height);
+    }
+    public double diagonal (){
+        return Math.sqrt(widht * widht + height * height);
+    }
+}
---
+++ src/exercises/orientedObjectProgramming/exercise2_oop/program/Program.java
@@ -0,0 +1,24 @@
+package exercises.orientedObjectProgramming.exercise2_oop.program;
+
+import exercises.orientedObjectProgramming.exercise2_oop.entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        Rectangle rect = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rect.widht = scanner.nextDouble();
+        rect.height = scanner.nextDouble();
+
+        System.out.printf("AREA: %.2f%n", rect.area());
+        System.out.printf("PERIMETER: %.2f%n", rect.perimeter());
+        System.out.printf("DIAGONAL: %.2f%n", rect.diagonal());
+        scanner.close();
+    }
+}
